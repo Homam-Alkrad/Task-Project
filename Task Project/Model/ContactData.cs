44f80ef8df6fba525774e8ddbdafd39cacf82642
user <user@DESKTOP-F28UUKO>
2024-05-07 14:41:30 +0300

    Add project files.

---
+++ Task Project/Model/ContactData.cs
@@ -0,0 +1,16 @@
+﻿using Task_Project.Model;
+
+public static class ContactData
+{
+	public static List<Contact> Contacts { get; private set; } = new List<Contact>();
+
+	static ContactData()
+	{
+        Contacts = new List<Contact>
+        {
+            new Contact { Id = 1, FirstName = "Homam", LastName = "Ahmad", Email = "alkurdihom@gmail.com", PhoneNumber = "0791089960" },
+            new Contact { Id = 2, FirstName = "Qasem", LastName = "Alkrad", Email = "h.alkrad.dev@gmail.com", PhoneNumber = "0797412338" },
+            new Contact { Id = 3, FirstName = "Sami", LastName = "Waledd", Email = "homam@gmail.com", PhoneNumber = "07863254" }
+        };
+    }
+}
---
+++ Task Project/Model/ContactService.cs
@@ -0,0 +1,36 @@
+﻿using Task_Project.Model;
+
+public static class ContactService
+{
+	public static void AddContact(Contact contact)
+	{
+		contact.Id = ContactData.Contacts.Count + 1; // Generate unique ID
+		ContactData.Contacts.Add(contact);
+	}
+
+	public static List<Contact> GetContacts()
+	{
+		return ContactData.Contacts;
+	}
+
+	public static void UpdateContact(Contact contact)
+	{
+		var existingContact = ContactData.Contacts.FirstOrDefault(c => c.Id == contact.Id);
+		if (existingContact != null)
+		{
+			existingContact.FirstName = contact.FirstName;
+			existingContact.LastName = contact.LastName;
+			existingContact.Email = contact.Email;
+			existingContact.PhoneNumber = contact.PhoneNumber;
+		}
+	}
+
+	public static void DeleteContact(int id)
+	{
+		var contactToRemove = ContactData.Contacts.FirstOrDefault(c => c.Id == id);
+		if (contactToRemove != null)
+		{
+			ContactData.Contacts.Remove(contactToRemove);
+		}
+	}
+}
